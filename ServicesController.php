74012a206f27b2095a0b67f239f7ec2a9f76e8da
liberspaces <82107021+liberspaces@users.noreply.github.com>
2023-06-26 14:00:48 +0200

    Update ServicesController.php

--- ServicesController.php
+++ ServicesController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Job\Http\Controllers;
 
-use App\Models\Service;
+use Job\Models\Service;
 use Illuminate\Http\Request;
 
 class ServicesController extends Controller
